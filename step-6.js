812c4e9177fd6d7a3e9f7cfc3a437ee695b64fa4
Lucia Cerchie <luciacerchie@gmail.com>
2024-05-29 11:08:28 -0700

    Update step-6.js

--- step-6.js
+++ step-6.js
@@ -1,46 +1,36 @@
-       socket.on("event", function (message) {
+      [1] socket.on("event", function (message) {
 
-         progress_bar_id = `${message.message.question_id}`;
+        [2] progress_bar_id = `${message.message.question_id}`;
 
-         let progressBar = document.getElementById(
+        [3] let progressBar = document.getElementById(
           `${progress_bar_id}-progress`,
         );
 
-         let labelForBar = document.getElementById(`${progress_bar_id}-label`);
-
+        [4] let labelForBar = document.getElementById(`${progress_bar_id}-label`);
+         let max = Object.values(message.message.count)[0] + Object.values(message.message.count)[1];
         //if length is one then ignore as result of retraction in potential FlinkSQL developments in later versions
         if (Object.keys(message.message.count).length == 1) {
           //don't do anything
-         } else if (
+       [5] } else if (
           Object.values(message.message.count)[0] >
           Object.values(message.message.count)[1]
         ) {
           value = Object.values(message.message.count)[0];
-          max =
-            Object.values(message.message.count)[0] +
-            Object.values(message.message.count)[1];
-          progressBar.setAttribute("value", value);
-          progressBar.setAttribute("max", max);
          [a] labelForBar.innerHTML = `${Object.keys(message.message.count)[0]} wins  with ${Object.values(message.message.count)[0]} votes out of ${max}`;
-        } else if (
+        [6] } else if (
           Object.values(message.message.count)[0] ===
           Object.values(message.message.count)[1]
         ) {
           value = Object.values(message.message.count)[0];
-          max =
-            Object.values(message.message.count)[0] +
-            Object.values(message.message.count)[1];
-          progressBar.setAttribute("value", value);
-          progressBar.setAttribute("max", max);
-         [b] labelForBar.innerHTML = `It's a tie! There were ${max} total votes`;
-         } else {
+         [a] labelForBar.innerHTML = `It's a tie! There were ${max} total votes`;
+       [7] } else {
           value = Object.values(message.message.count)[1];
-          max =
-            Object.values(message.message.count)[0] +
-            Object.values(message.message.count)[1];
-          progressBar.setAttribute("value", value);
-          progressBar.setAttribute("max", max);
-         [c] labelForBar.innerHTML = `${Object.keys(message.message.count)[1]} wins  with ${Object.values(message.message.count)[1]} votes out of ${max}`;
+         [a] labelForBar.innerHTML = `${Object.keys(message.message.count)[1]} wins  with ${Object.values(message.message.count)[1]} votes out of ${max}`;
         }
-        showProgressBar(progress_bar_id);
+       [a] progressBar.setAttribute("value", value);
+       [a] progressBar.setAttribute("max", max);
+        if (clicked === true)
+       { showProgressBar(progress_bar_id);
+        clicked = false;
+       }
       });
